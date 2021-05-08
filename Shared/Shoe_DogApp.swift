3a3d9817f7c04268fb52bf446a0dac2d58df74b7
sxc <qiuyequ@icloud.com>
2021-05-08 11:04:36 +0800

    Initial Commit

---
+++ Shared/Shoe_DogApp.swift
@@ -0,0 +1,17 @@
+//
+//  Shoe_DogApp.swift
+//  Shared
+//
+//  Created by Xiaochun Shen on 2021/5/8.
+//
+
+import SwiftUI
+
+@main
+struct Shoe_DogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
